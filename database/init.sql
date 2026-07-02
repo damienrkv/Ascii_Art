00fb81e1636aa6c8feeda706a50b6e72cd0f2bb3
Damien <damienrajkovic.neu@gmail.com>
2026-07-02 18:02:36 +0200

    LONTEXT change from TEXT in table definition

--- database/init.sql
+++ database/init.sql
@@ -3,7 +3,7 @@ USE ascii_art;
 CREATE TABLE IF NOT EXISTS uploads (
     id INT AUTO_INCREMENT PRIMARY KEY,
     image_path VARCHAR(255) NOT NULL,
-    ascii_art TEXT NOT NULL,
+    ascii_art LONGTEXT NOT NULL,
     num_chars INT NOT NULL,
     upload_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
